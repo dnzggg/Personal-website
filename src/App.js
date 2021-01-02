f0ee5248de6576da12b72217138c581ded26f904
bacxhus <enricxpieri@gmail.com>
2021-01-02 01:01:51 +0000

    UI-1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import {Route, Router} from "react-router-dom";
 import React from "react";
 import Home from "./Components/Home";
@@ -6,21 +6,11 @@ import history from "./utils/history."
 
 const App = () => {
   return (
-    <div className="App">
       <Router history={history}>
-        <div className="Index">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/Home">
-            <Home/>
-          </Route>
-        </div>
       </Router>
-    </div>
   );
 };
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,60 @@ import React from "react";
 
 
 const Home = () => {
-  return(
-      <div className="Name">
-          <p>
-              Deniz Gorur
-          </p>
+  return <main>
+
+    <section className="hero">
+      <div className="hero-content">
+        <h1>&#60;Hello, my name is Deniz. I’m <br /> a button presser./&gt;</h1>
+        <p>I write code, think about data, and create digital experiences.
+          <br />
+          Currently Senior Journalist-Engineer at The Pudding.
+          <br />
+          <br />
+          Listen to podcasts I've been on, or read articles I've written.</p>
       </div>
-  )
+    </section>
+
+    <section className="content">
+
+      <section className="skills">
+        <div className="divider">
+          <hr />
+        </div>
+
+        <h2>SKILLS</h2>
+
+        <div className="project-content">
+          <div>
+            <h3>Programming</h3>
+            <p>This is an overview of my current skillset that I am constantly blah blah and this is some filler text to make it look likw aomwrhinf ahouls go hwew and sometjing else.</p>
+          </div>
+          <div className="template"></div>
+        </div>
+
+        <h3>Software and Tools</h3>
+        <ul>
+          <li className="tools">Visualisation of Data</li>
+          <li className="tools">Data Handling and Analysis</li>
+          <li className="tools">GUI</li>
+          <li className="tools">API</li>
+          <li className="tools">React</li>
+          <li className="tools">PyCharm</li>
+          <li className="tools">Eclipse</li>
+          <li className="tools">WebStorm</li>
+          <li className="tools">Visual Studio Code</li>
+          <li className="tools">Office</li>
+          <li className="tools">Version Control Systems</li>
+
+        </ul>
+
+      </section>
+
+    </section>
+
+
+
+  </main>
 };
 
 export default Home;
