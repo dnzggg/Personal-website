f9f139835995c59cd5fab7a1ba3c1c17e181dbec
dnzggg <denizgorur75@gmail.com>
2021-01-10 00:31:10 +0000

    Burger menu added for mobile

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,12 +3,15 @@ import history from "../utils/history"
 import "./NavBar.scss"
 import Dropdown from "./Dropdown";
 import scrollToRef from "../function/scrollToRef"
+import { ReactComponent as Menu } from "../images/menu.svg"
+import { ReactComponent as Close } from "../images/close.svg"
 
 const NavBar = () => {
   const height = "1%";
 
   const [scrollTop, setScrollTop] = useState(window.scrollY);
   const [hide, setHide] = useState("show");
+  const [mobileHide, setMobileHide] = useState("hide");
   const [section, setSection] = useState("about")
 
   useEffect(() => {
@@ -42,26 +45,66 @@ const NavBar = () => {
     setScrollTop(window.scrollY);
   };
 
+  const handleOpen = () => {
+    if (mobileHide === "hide") {
+      document.body.classList.add("mobile-menu-open")
+      setMobileHide("show")
+    } else {
+      document.body.classList.remove("mobile-menu-open")
+      setMobileHide("hide")
+    }
+  }
+
+  const handleClick = (e) => {
+    handleOpen()
+
+    scrollToRef(e)
+  }
+
   let list = ["Cooperative Strategies in Multi-agent Systems", "Restaurant App", "Calculator",
     "Tower Defence", "Self Pong Playing Robot", "Snake", "Minesweeper", "Tic Tac Toe"];
 
   return (
-    <nav className={"navbar-sticky navbar-" + hide}>
-      <div className="content nav-items" style={{ height: height }}>
-        <button className="logo" onClick={() => history.push('/')}>Deniz</button>
-        <div className="nav-links">
-          <button className="section" style={section === "about" ? { textDecorationLine: 'underline' } : {}}
-            onClick={scrollToRef}>About</button>
-          <button className="section" style={section === "skills" ? { textDecorationLine: 'underline' } : {}}
-            onClick={scrollToRef}>Skills</button>
-          <Dropdown header={"Projects"} list={list} section={section} />
-          <button className="section" style={section === "contact" ? { textDecorationLine: 'underline' } : {}}
-            onClick={scrollToRef}>Contact</button>
+    <div>
+      <nav className={"navbar-desktop navbar-" + hide}>
+        <div className="content nav-desktop-items" style={{ height: height }}>
+          <button className="logo" onClick={() => history.push('/')}>Deniz</button>
+          <div className="nav-desktop-links">
+            <button className="section" style={section === "about" ? { textDecorationLine: 'underline' } : {}}
+                    onClick={scrollToRef}>About</button>
+            <button className="section" style={section === "skills" ? { textDecorationLine: 'underline' } : {}}
+                    onClick={scrollToRef}>Skills</button>
+            <Dropdown header={"Projects"} list={list} section={section} />
+            <button className="section" style={section === "contact" ? { textDecorationLine: 'underline' } : {}}
+                    onClick={scrollToRef}>Contact</button>
+          </div>
+        </div>
+      </nav>
+
+
+      <nav className={"navbar-mobile"}>
+        <div className="nav-mobile-items" style={{ height: height }}>
+          <div className="logo" />
+          <button className={"logo"} onClick={handleOpen}>
+            <Menu className={"svg"} />
+          </button>
         </div>
 
+        <div className={"nav-menu-mobile nav-menu-mobile-" + mobileHide}>
+          <div className={"nav-mobile-items"}>
+            <div />
+            <button className={"exit"} onClick={handleOpen}>
+              <Close className={"svg"} />
+            </button>
+          </div>
+          <button className="section" onClick={handleClick}>About</button>
+          <button className="section" onClick={handleClick}>Skills</button>
+          <Dropdown header={"Projects"} list={list} section={section} />
+          <button className="section" onClick={handleClick}>Contact</button>
+        </div>
+      </nav>
+    </div>
 
-      </div>
-    </nav>
   )
 }
 
--- src/function/scrollToRef.js
+++ src/function/scrollToRef.js
@@ -2,7 +2,7 @@
 const scrollToRef = (e) => {
     const id = e.target.innerText.toLowerCase()
     const y = document.getElementById(id).getBoundingClientRect().y + window.scrollY
-    window.scrollTo({top: y,behavior: "smooth"})
+    window.scroll({top: y,behavior: "smooth"})
 }
 
 export default scrollToRef
